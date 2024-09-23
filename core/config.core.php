48fef1fdc1f3ee019bbe2c1909505c46b0deda4f
kongkiat.b <kongkiat.b@srisawadpower.com>
2024-09-23 20:40:25 +0700

    Set DB UAT & IMG

---
+++ core/config.core.php
@@ -0,0 +1,12 @@
+<?php
+// MySQL hostname
+define('DB_HOST', 'localhost:3306');
+
+// MySQL database username
+define('DB_USERNAME', 'systemit_uat_mts');
+
+// MySQL database password
+define('DB_PASSWORD', 'R6?is603t');
+
+// MySQL database name
+define('DB_NAME', 'systemit_uat_mts');
